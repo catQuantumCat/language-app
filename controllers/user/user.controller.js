8c077a81abe0436dda367738ec28790d4c0a332d
{Tran Bach Tung} <tung2982004@gmail.com>
2025-05-16 22:49:57 +0700

    Import mongoose

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -1,6 +1,6 @@
 const User = require('../../models/user/user.model');
 const bcrypt = require('bcrypt');
-
+const mongoose = require('mongoose');
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
